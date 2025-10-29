9b2bcd8de093534ee59694c5aafd62ca4300f7fa
Mravacardo <matthewheathcote@icloud.com>
2025-10-29 16:52:15 +0000

    Main.py

---
+++ Main.py
@@ -0,0 +1,23 @@
+mylist = [10, 3, 6, 2, 8, 1, 7, 5, 9, 4]
+# mylist.sort(reverse = True)
+# print(mylist)
+# mylist.sort(reverse = False)
+# print(mylist)
+#bubble sort
+# for i in range(0, len(mylist)):
+#     for j in range(i, len(mylist)):
+#       if mylist[i] < mylist[j]:
+#           mylist[i],mylist[j] = mylist[j], mylist[i]
+
+# print(mylist)
+#insertion sort
+# for i in range(0, len(mylist)):
+#   minElement = 10000000
+#   minLocation = -1
+#   for j in range(i, len(mylist)):
+#     if minElement > mylist[j]:
+#       minElement = mylist[j]
+#       minLocation = j
+#       mylist[i],mylist[j] = mylist[j], mylist[i]
+
+# print(mylist)
